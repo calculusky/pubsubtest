90913947391813504cc6df59ccd1aa67bcf2bbe8
calculusky <calculusky@gmail.com>
2022-10-05 22:29:19 +0100

    app

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var express = require('express')
-var app = express()
+var app = express();
+
+app.use(express.json());
 
 app.get('/', function (req, res) {
+  console.log()
   res.send('Hello World!')
+})
+
+app.post('/', function (req, res) {
+  console.log(req.body, '******************** res body *******')
+  res.send('Hello World Post!')
 })
 
 app.listen(3000, function () {
